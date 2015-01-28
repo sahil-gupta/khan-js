21cc500eed8d4295c560e03dccd753e1ef3f8b21
Sahil Gupta <sahil.gupta@yale.edu>
2015-01-28 02:08:35 -0500

    review acorn

--- hello-world.js
+++ hello-world.js
@@ -1,3 +1,16 @@
 // sahil gupta
 
+/*
+why acorn over esprima
+both parse javascript and get the job done
+both produce well-documented, beautiful ASTs
+both have optional 'loose mode' where tolerates errors
+
+but by esprima's own benchmarks (speed-test.png)
+    acorn is midly faster
+acorn half the lines of code
+
+
+*/
+
 console.log('hi');
