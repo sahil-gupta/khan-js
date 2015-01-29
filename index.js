fcccb17930db1ae005b6dae6904620757c694bc2
Sahil Gupta <sahil.gupta@yale.edu>
2015-01-29 07:52:55 -0500

    implement whitelist/blacklist

--- index.js
+++ index.js
@@ -17,19 +17,28 @@ var dict = {
     'break'     : 'BreakStatement'
 }
 
-// methods and arguments
+
+// methods and arguments required by user
 var methods = {
-    whitelist: {'for': 1, 'var': 1},
-    blacklist: {'while': 0},
-    structure: {'for': 'if'}
+    whitelist: {},
+    blacklist: {},
+    structure: {}
 };
+
 
 // input node of tree to traverse, apply function, recurse
+// modifies methods{}
 var treeTraverse = function(node, f) {
     f(node);
-    // hereee recurse
 
+    // for each child node or array of nodes, recurse
+    var key, child;
+    for (key in node) {
+        if (node.hasOwnProperty(key) && (child = node[key]) instanceof Object)
+            $.each(child, function(i, node) { treeTraverse(node, f); });
+    }
 };
+
 
 
 // input syntax tree
@@ -43,6 +52,7 @@ var testAPI = function(node) {
     
     // function checks node against each whitelist/blacklist keyword
     var checkNode = function(node) {
+        var key;
         for (key in methods.whitelist) {
             if (dict[key] === node.type)
                 methods.whitelist[key]--;       // keyword seen
@@ -54,7 +64,7 @@ var testAPI = function(node) {
         }
     };
 
-    treeTraverse(node, checkNode);
+    treeTraverse(node, checkNode);              // traverse tree w checkNode()
 
     return ['heree'];
 };
@@ -72,13 +82,20 @@ $(function() {
         if(code >= 37 && code <= 40)
             return;
 
+        methods = {                                     // update methods
+            whitelist: {'for': 1, 'var': 1},
+            blacklist: {'while': 0},
+            structure: {'for': 'if'}
+        };
+
+
         var text = $('#text').val(),               // get textarea code
             tree = esprima.parse(text);
 
         $('#tree').val(JSON.stringify(tree, null, 4));  // show tree
 
         var feedback = testAPI(tree);
-        console.log(feedback);
+        // console.log(feedback); // heree
     });
 
 });
