7d3218fa611b2b077f66d97079ef6ef92da82af6
Sahil Gupta <sahil.gupta@yale.edu>
2015-01-29 17:50:26 -0500

    fix tree traverse white blacklist

--- index.js
+++ index.js
@@ -15,15 +15,45 @@ var dict = {
     'while'     : 'WhileStatement',
     'for'       : 'ForStatement',
     'break'     : 'BreakStatement'
-}
+    },
+    methods = { whitelist: {},
+                blacklist: {},
+                structure: {} };    // methods and arguments required by user
 
 
-// methods and arguments required by user
-var methods = {
-    whitelist: {},
-    blacklist: {},
-    structure: {}
+// ignore arrow keys
+var arrowKey = function(e) {
+    var code = (e.keyCode || e.which);
+    return (code >= 37 && code <= 40)
 };
+
+
+// input key event, list type as string
+var updateList = function(e, list) {
+    if (arrowKey(e)) return;
+
+    var input;
+    if (list === 'whitelist') {
+        input = $('#inputW');
+        methods.whitelist = {};
+    } else if (list === 'blacklist') {
+        input = $('#inputB');
+        methods.blacklist = {};
+    }
+
+    input = input.val().split(/[\s,]+/);    // split and trim
+
+    input.forEach(function(key) {           // update methods{}
+        if (list === 'whitelist') {
+            if (methods.whitelist[key])
+                methods.whitelist[key]++;   // key should have freq++
+            else
+                methods.whitelist[key] = 1;
+        } else if (list === 'blacklist') {
+            methods.blacklist[key] = 0;     // key should have freq 0
+        }
+    });
+}
 
 
 // input node of tree to traverse, apply function, recurse
@@ -34,8 +64,12 @@ var treeTraverse = function(node, f) {
     // for each child node or array of nodes, recurse
     var key, child;
     for (key in node) {
-        if (node.hasOwnProperty(key) && (child = node[key]) instanceof Object)
-            $.each(child, function(i, node) { treeTraverse(node, f); });
+        if (node.hasOwnProperty(key) && (child = node[key]) instanceof Object){
+            if (child instanceof Array)
+                child.forEach(function(node) { treeTraverse(node, f); });
+            else
+                treeTraverse(child, f);            
+        }
     }
 };
 
@@ -66,36 +100,59 @@ var testAPI = function(node) {
 
     treeTraverse(node, checkNode);              // traverse tree w checkNode()
 
-    return ['heree'];
+    // loop through lists, make sure keys have correct frequency
+    var feedbackW = [], feedbackB = [], key;
+    for (key in methods.whitelist) {
+        if (methods.whitelist[key] > 0)         // if not enough were seen
+            feedbackW.push(key);
+    }
+    for (key in methods.blacklist) {
+        if (methods.blacklist[key] > 0)         // if too many were seen
+            feedbackB.push(key);
+    }
+    feedbackW = feedbackW.length === 0 ? 'looks good' :
+                                            'need ' + feedbackW.join(' + ');
+    feedbackB = feedbackB.length === 0 ? 'looks good' :
+                                            'avoid ' + feedbackB.join(' + ');
+
+    return {w: feedbackW, b: feedbackB};
 };
 
 
 // document ready
 $(function() {
-    // initial textarea value
-    var temp = 'var x = 2 * 3;';
-    $('#text').val(temp);
+    // update methods{} when inputs change
+    $('#inputW').bind('change keyup', function(e) {
+        updateList(e, 'whitelist');
+    });
+    $('#inputB').bind('change keyup', function(e) {
+        updateList(e, 'blacklist');
+    });
+
+
 
     // run tests when textarea changes
     $('#text').bind('change keyup', function(e) {
-        var code = (e.keyCode || e.which);              // ignore arrow keys
-        if(code >= 37 && code <= 40)
-            return;
+        arrowKey(e);
 
-        methods = {                                     // update methods
-            whitelist: {'for': 1, 'var': 1},
-            blacklist: {'while': 0},
-            structure: {'for': 'if'}
-        };
+            //structure: {'for': 'if'}
 
+        // check whitelist and blacklist mutually exclusive
+        var keyW, keyB;
+        for (keyW in methods.whitelist) {
+            for (keyB in methods.blacklist) {
+                if (keyW === keyB)
+                    return $('#feedback').val('whitelist overlaps blacklist');
+            }
+        }
+        // assert whitelist and blacklist mutex
 
-        var text = $('#text').val(),               // get textarea code
+        var text = $('#text').val(),                // get textarea code
             tree = esprima.parse(text);
 
-        $('#tree').val(JSON.stringify(tree, null, 4));  // show tree
-
+        // show feedback
         var feedback = testAPI(tree);
-        // console.log(feedback); // heree
+        $('#feedbackW').text(feedback.w);
+        $('#feedbackB').text(feedback.b);
     });
-
 });
